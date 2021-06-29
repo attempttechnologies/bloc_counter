1ec65d5b3010d89a04cd3d81f7dd0cc3e5db8e17
Sulejman Limaj <limaj.sulejman@gmail.com>
2021-06-29 04:39:38 +0200

    initial commit

---
+++ lib/counter_observer.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+
+/// {@template counter_observer}
+/// [BlocObserver] for the counter application which
+/// observes all state changes.
+/// {@endtemplate}
+
+class CounterObserver extends BlocObserver {
+  @override
+  void onChanged(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+  
+}
